66654c470155660619701a7c56d82f0c17ca28e2
TechnologyDuckee <jacobpalmer1@live.com>
2025-11-18 19:27:13 -0800

    build character_count function and imported to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count
+from stats import get_character_count
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -9,3 +10,8 @@ def get_book_text(file_path):
 book_text = get_book_text("books/frankenstein.txt")
 word_count = get_word_count(book_text)
 print(f"Found {word_count} total words.")
+
+character_count = get_character_count(book_text)
+print("Character counts:")
+for ch, count in character_count.items():
+    print(f"'{ch}': {count}")
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+def get_character_count(text):
+    import string
+
+    lower_text = text.lower()
+    # initialize dictionary for all lowercase letters a-z with 0
+    counts = {ch: 0 for ch in string.ascii_lowercase}
+
+    # count only lowercase alphabetic characters
+    for ch in lower_text:
+        if ch in counts:
+            counts[ch] += 1
+
+    return counts
