841f65eac443079e9e74c2682bb0a8b7ab9f67f2
TechnologyDuckee <jacobpalmer1@live.com>
2025-11-18 19:51:36 -0800

    built sorter by number and print statements to make a report

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
+def sort_on_count(item):
+    return item[1]
+
+
 book_text = get_book_text("books/frankenstein.txt")
 word_count = get_word_count(book_text)
-print(f"Found {word_count} total words.")
-
 character_count = get_character_count(book_text)
+# sort character counts by count value (dict -> list of (ch,count))
+character_items = list(character_count.items())
+character_items.sort(key=sort_on_count, reverse=True)
+
+# print reports
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {file_path} ...")
+print("----------- Word Count ----------")
+print(f"Found {word_count} total words.")
+print("-------- Character Count --------")
 print("Character counts:")
-for ch, count in character_count.items():
-    print(f"'{ch}': {count}")
+for ch, count in character_items:
+    print(f"  {ch}: {count}")
